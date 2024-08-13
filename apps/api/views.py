b40ad550494ed8d9f7a9d76b5c796f6882d281ce
administrador <administrador@ubuntusystem.myguest.virtualbox.org>
2024-08-13 17:46:05 -0400

    Inicio Backend

---
+++ apps/api/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.db.models import Sum
+# Restframework
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
+
+#from drf_yasg import openapi
+#from drf_yasg.utils import swagger_auto_schema
+from datetime import datetime
+
+# Others
+import json
+import random
+
+# Custom Imports
+from apps.api import models as api_models
+from apps.api import serializers as api_serializer
+
+class MyTokenObtainePairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = api_models.User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
