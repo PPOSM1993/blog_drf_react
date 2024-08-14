d3a8cf7c3f1f90ae5402572257763b8e7249abe6
administrador <administrador@ubuntusystem.myguest.virtualbox.org>
2024-08-14 18:49:51 -0400

    finalizacion backend e inicio frontend

---
+++ apps/api/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from apps.api import views as api_views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('user/token/', api_views.MyTokenObtainePairView.as_view(), name='token_obtain_pair'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', api_views.RegisterView.as_view(), name='auth_register'),
+    path('user/profile/<user_id>/', api_views.ProfileView.as_view(), name='user_profile'),
+
+    #Post Endpoint
+    path('post/category/list/', api_views.CategoryListAPIView.as_view()),
+    path('post/category/posts/<category_slug>/', api_views.PostCategoryListAPIView.as_view()),
+    path('post/list/', api_views.PostListAPIView.as_view()),
+    path('post/detail/<slug>/', api_views.PostDetailAPIView.as_view()),
+    path('post/like-post/', api_views.LikePostAPIView.as_view()),
+    path('post/comment-post/', api_views.PostCommentAPIView.as_view()),
+    path('post/bookmark-post/', api_views.BookmarkPostAPIView.as_view()),
+
+    path('author/dashboard/stats/<user_id>/', api_views.DashboardStats.as_view()),
+    path('author/dashboard/comment-list/', api_views.DashboardCommentLists.as_view()),
+    path('author/dashboard/noti-list/<user_id>/', api_views.DashboardNotificationLists.as_view()),
+    path('author/dashboard/noti-mark-seen/', api_views.DashboardMarkNotificationAsSeen.as_view()),
+    path('author/dashboard/reply-comment/', api_views.DashboardReplyCommentAPIView.as_view()),
+    path('author/dashboard/post-create/', api_views.DashboardPostCreateAPIView.as_view()),
+    path('author/dashboard/post-detail/<user_id>/<post_id>/', api_views.DashboardPostEditAPIView.as_view()),
+
+]
